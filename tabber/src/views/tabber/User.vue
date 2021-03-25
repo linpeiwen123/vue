e8e9f2e9a2cf08de01709ff9b63eb319ee1b540e
linpeiwen <2576829898@qq.com>
2021-03-25 18:31:05 +0800

    aa

--- tabber/src/views/tabber/User.vue
+++ tabber/src/views/tabber/User.vue
@@ -9,9 +9,9 @@
     <div>{{$store.state.info}}</div>
 
     <button @click="fiveFun(5)">+5</button>
-    <button @click="tenFun(10)" >+10</button>
-    <button @click="userPushFun" >添加</button>
-    <button @click="infoSet" >修改</button>
+    <button @click="tenFun(10)">+10</button>
+    <button @click="userPushFun">添加</button>
+    <button @click="infoSet">修改</button>
   </div>
 </template>
 
@@ -23,27 +23,41 @@
     USERPUSH,
     SETINFO
   } from "../../store/mutations-type"
-    export default {
-      name: "User",
-      methods:{
-        fiveFun(count){
-          this.$store.commit(ADDCONT,count)
-        },
-        tenFun(count){
-          this.$store.commit(ADDCONT,count)
-          // let aaa  =  {id:"116",name:"jack",age:35}
-          // this.$store.commit("userPush",aaa)
-        },
-        userPushFun(){
-          let use  =  {id:"116",name:"jack",age:35}
-          this.$store.commit(USERPUSH,use)
-        },
-        infoSet(){
-          //异步调用，必须调用actions里面的函数
-          this.$store.dispatch("asynInfo")
-        }
+
+  export default {
+    name: "User",
+    methods: {
+      fiveFun(count) {
+        this.$store.commit(ADDCONT, count)
+      },
+      tenFun(count) {
+        this.$store.commit(ADDCONT, count)
+        // let aaa  =  {id:"116",name:"jack",age:35}
+        // this.$store.commit("userPush",aaa)
+      },
+      userPushFun() {
+        let use = {id: "116", name: "jack", age: 35}
+        this.$store.commit(USERPUSH, use)
+      },
+      infoSet() {
+        // 1、异步调用，必须调用actions里面的函数
+        // this.$store.dispatch("asynInfo")
+
+        // 2、携带信息
+        // this.$store.dispatch("asynInfo", {
+        //   message: "成功了",
+        //   success: () => {
+        //       console.log("里面的也成功了")
+        //   }
+        // })
+
+        // 3.Promise调用then返回
+        this.$store.dispatch("asynInfo", "我是携带者").then(success => {
+          console.log(success)
+        })
       }
     }
+  }
 </script>
 
 <style scoped>
